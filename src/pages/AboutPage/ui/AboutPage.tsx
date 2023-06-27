3f5481da7fa14a681c5bdd9b5cfb09a3973d48c1
l1msn <Mydarlingg99>
2023-06-28 00:14:52 +0300

    Added StyleLint and plugin i18n for eslint

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -3,7 +3,7 @@ import React, {JSX} from 'react';
 const AboutPage: React.FC = (): JSX.Element => {
   return (
     <div>
-          About Page
+
     </div>
   );
 };
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -3,7 +3,7 @@ import React, {JSX} from 'react';
 const MainPage: React.FC = (): JSX.Element => {
   return (
     <div>
-          Main Page
+
     </div>
   );
 };
