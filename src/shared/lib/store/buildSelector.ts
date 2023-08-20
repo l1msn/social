389ec2f441ba501b9a436dd73f436effff0e8c4e
l1msn <64272568+l1msn@users.noreply.github.com>
2023-08-20 15:56:05 +0300

    Modified buildSelector hook function(added more types)

--- src/shared/lib/store/buildSelector.ts
+++ src/shared/lib/store/buildSelector.ts
@@ -1,12 +1,18 @@
 import { IStateSchema } from '@/app/providers/StoreProvider';
 import { useSelector } from 'react-redux';
 
-type Selector<T> = (state: IStateSchema) => T;
-type Result<T> = [() => T, Selector<T>];
+type Selector<T, Args extends any[]> = (
+    state: IStateSchema,
+    ...args: Args
+) => T;
+type Hook<T, Args extends any[]> = (...args: Args) => T;
+type Result<T, Args extends any[]> = [Hook<T, Args>, Selector<T, Args>];
 
-function buildSelector<T>(selector: Selector<T>): Result<T> {
-    const useSelectorHook = () => {
-        return useSelector(selector);
+function buildSelector<T, Args extends any[]>(
+    selector: Selector<T, Args>,
+): Result<T, Args> {
+    const useSelectorHook: Hook<T, Args> = (...args: Args) => {
+        return useSelector((state: IStateSchema) => selector(state, ...args));
     };
 
     return [useSelectorHook, selector];
