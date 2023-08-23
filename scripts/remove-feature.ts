47e6eb168c2143f548eae6d7409dd18e59e600ae
l1msn <64272568+l1msn@users.noreply.github.com>
2023-08-24 02:44:08 +0300

    Modified jsonSettings of user Added greeting modal for articles page Added HOC withTheme Fixed scripts for ui report Added script to delete features by flag

--- scripts/remove-feature.ts
+++ scripts/remove-feature.ts
@@ -1,9 +1,18 @@
-import { Node, Project, SyntaxKind } from 'ts-morph';
+import { JsxAttribute, Node, Project, SyntaxKind } from 'ts-morph';
 
 const project = new Project({});
 
+// project.addSourceFilesAtPaths('src/**/*.ts');
+// project.addSourceFilesAtPaths('src/**/*.tsx');
+
+project.addSourceFilesAtPaths('src/**/ArticleDetailsPage.ts');
+project.addSourceFilesAtPaths('src/**/ArticleDetailsPage.tsx');
+
 const removedFeatureName = process.argv[2];
 const featureState = process.argv[3];
+
+const toggleFunctionName = 'toggleFeatures';
+const toggleComponentName = 'ToggleFeatures';
 
 if (!removedFeatureName) {
     throw new Error('No removed feature name');
@@ -13,9 +22,6 @@ if (!featureState && featureState !== 'on' && featureState !== 'off') {
     throw new Error('No removed feature state (on or off)');
 }
 
-project.addSourceFilesAtPaths('src/**/*.ts');
-project.addSourceFilesAtPaths('src/**/*.tsx');
-
 const files = project.getSourceFiles();
 
 function isToggleFunction(node: Node) {
@@ -24,7 +30,7 @@ function isToggleFunction(node: Node) {
     node.forEachChild((child) => {
         if (
             child.isKind(SyntaxKind.Identifier) &&
-            child.getText() === 'ToggleFeatures'
+            child.getText() === toggleFunctionName
         ) {
             isToggleFeatures = true;
         }
@@ -33,43 +39,105 @@ function isToggleFunction(node: Node) {
     return isToggleFeatures;
 }
 
+function isToggleComponent(node: Node) {
+    const identifier = node.getFirstDescendantByKind(SyntaxKind.Identifier);
+
+    return identifier?.getText() === toggleComponentName;
+}
+
+function replaceToggleFunction(node: Node) {
+    const objectOptions = node.getFirstDescendantByKind(
+        SyntaxKind.ObjectLiteralExpression,
+    );
+
+    if (!objectOptions) return;
+
+    const onFunctionProperty = objectOptions.getProperty('on');
+    const offFunctionProperty = objectOptions.getProperty('off');
+    const featureNameProperty = objectOptions.getProperty('name');
+
+    const onFunction = onFunctionProperty?.getFirstDescendantByKind(
+        SyntaxKind.ArrowFunction,
+    );
+    const offFunction = offFunctionProperty?.getFirstDescendantByKind(
+        SyntaxKind.ArrowFunction,
+    );
+    const featureName = featureNameProperty
+        ?.getFirstDescendantByKind(SyntaxKind.ArrowFunction)
+        ?.getText()
+        .slice(1, -1);
+
+    if (featureName !== removedFeatureName) return;
+
+    if (featureName === 'off') {
+        node.replaceWithText(offFunction?.getBody().getText() ?? '');
+    }
+
+    if (featureName === 'on') {
+        node.replaceWithText(onFunction?.getBody().getText() ?? '');
+    }
+}
+
+const getAttributeNodeByName = (
+    jsxAttributes: JsxAttribute[],
+    name: string,
+) => {
+    return jsxAttributes.find(
+        (node: JsxAttribute) => node.getNameNode().getText() === name,
+    );
+};
+
+const getReplacedComponent = (attribute?: JsxAttribute) => {
+    const value = attribute
+        ?.getFirstDescendantByKind(SyntaxKind.JsxExpression)
+        ?.getExpression()
+        ?.getText();
+
+    if (value?.startsWith('(')) {
+        return value.slice(1, -1);
+    }
+
+    return value;
+};
+
+const replaceComponent = (node: Node) => {
+    const attributes = node.getDescendantsOfKind(SyntaxKind.JsxAttribute);
+
+    const onAttribute = getAttributeNodeByName(attributes, 'on');
+    const offAttribute = getAttributeNodeByName(attributes, 'off');
+
+    const featureNameAttribute = getAttributeNodeByName(attributes, 'feature');
+    const featureName = featureNameAttribute
+        ?.getFirstDescendantByKind(SyntaxKind.StringLiteral)
+        ?.getText()
+        ?.slice(1, -1);
+
+    if (featureName !== removedFeatureName) return;
+
+    const offValue = getReplacedComponent(offAttribute);
+    const onValue = getReplacedComponent(onAttribute);
+
+    if (featureState === 'on' && onValue) {
+        node.replaceWithText(onValue);
+    }
+
+    if (featureState === 'off' && offValue) {
+        node.replaceWithText(offValue);
+    }
+};
+
 files.forEach((sourceFile) => {
     sourceFile.forEachDescendant((node) => {
         if (node.isKind(SyntaxKind.CallExpression) && isToggleFunction(node)) {
-            const objectOptions = node.getFirstDescendantByKind(
-                SyntaxKind.ObjectLiteralExpression,
-            );
-
-            if (!objectOptions) return;
-
-            const onFunctionProperty = objectOptions.getProperty('on');
-            const offFunctionProperty = objectOptions.getProperty('off');
-            const featureNameProperty = objectOptions.getProperty('name');
-
-            const onFunction = onFunctionProperty?.getFirstDescendantByKind(
-                SyntaxKind.ArrowFunction,
-            );
-            const offFunction = offFunctionProperty?.getFirstDescendantByKind(
-                SyntaxKind.ArrowFunction,
-            );
-            const featureName = featureNameProperty
-                ?.getFirstDescendantByKind(SyntaxKind.ArrowFunction)
-                ?.getText()
-                .slice(1, -1);
-
-            if (featureName !== removedFeatureName) return;
-
-            if (featureName === 'off') {
-                node.replaceWithText(offFunction?.getBody().getText() ?? '');
-            }
-
-            if (featureName === 'on') {
-                node.replaceWithText(onFunction?.getBody().getText() ?? '');
-            }
+            replaceToggleFunction(node);
+        }
+        if (
+            node.isKind(SyntaxKind.JsxSelfClosingElement) &&
+            isToggleComponent(node)
+        ) {
+            replaceComponent(node);
         }
     });
 });
 
 project.save();
-
-//10.43
--- src/shared/features/index.ts
+++ src/shared/features/index.ts
@@ -1,4 +1,5 @@
 import { getFeatureFlags, setFeatureFlags } from './setGetFeatures';
 import toggleFeatures from './toggleFeatures';
+import ToggleFeatures from './ToggleFeatures/ToggleFeatures';
 
-export { setFeatureFlags, getFeatureFlags, toggleFeatures };
+export { setFeatureFlags, getFeatureFlags, toggleFeatures, ToggleFeatures };
